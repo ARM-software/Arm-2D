ce728ebb4726622e98d462750a798a8d374d9e2d
Gabriel Wang <gabriel.wang@arm.com>
2022-09-05 22:23:58 +0100

    Update arm_2d.h

--- Library/Include/arm_2d.h
+++ Library/Include/arm_2d.h
@@ -22,8 +22,8 @@
  * Description:  Public header file to contain the all avaialble Arm-2D 
  *               interface header files 
  *
- * $Date:        09. Aug 2022
- * $Revision:    V.1.0.6
+ * $Date:        05. Sept 2022
+ * $Revision:    V.1.0.7
  *
  * Target Processor:  Cortex-M cores
  * -------------------------------------------------------------------- */
@@ -65,7 +65,7 @@ extern "C" {
 #define ARM_2D_VERSION_MAJOR        1                                           //!< Major version
 #define ARM_2D_VERSION_MINOR        1                                           //!< Minor version
 #define ARM_2D_VERSION_PATCH        0                                           //!< Patch number
-#define ARM_2D_VERSION_STR          "preview"                                   //!< tag
+#define ARM_2D_VERSION_STR          "preview1"                                  //!< tag
 
 /*!
  * \brief arm-2d version number in decimal
