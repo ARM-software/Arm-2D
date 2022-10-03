7e2904cfa47a7f7887398af80f8e1865fc1df90a
Z_Tam <2403626628@qq.com>
2022-10-03 21:17:02 +0800

    Update roller_list.c
    删除tab

--- examples/common/controls/roller_list.c
+++ examples/common/controls/roller_list.c
@@ -1,6 +1,5 @@
 #include "roller_list.h"
 
-
 //
 // LTOP_SPACE       **********
 // ******           *        * ******
@@ -11,68 +10,68 @@
 //
 
 /*
- *	øÿ÷∆≤Œ ˝
+ *	ÊéßÂà∂ÂèÇÊï∞
  */
-#define SPEED 5																	/* Ã¯÷°ÀŸ∂» */
-#define CONTROL_SCALE			0.67											/* –°‘≤Ω«/¥Û‘≤Ω« ±»¿˝ */
-#define CONTROL_SCALE_ALPHA		200												/* ÷–º‰Œª÷√µƒ≤ªÕ∏√˜∂» */
+#define SPEED 5                         /* Ë∑≥Â∏ßÈÄüÂ∫¶ */
+#define CONTROL_SCALE        0.67       /* Â∞èÂúÜËßí/Â§ßÂúÜËßí ÊØî‰æã */
+#define CONTROL_SCALE_ALPHA  200        /* ‰∏≠Èó¥‰ΩçÁΩÆÁöÑ‰∏çÈÄèÊòéÂ∫¶ */
 
-int8_t roller_switch = 0;		/* øÿ÷∆◊Û”“πˆ∂Ø */
+int8_t roller_switch = 0;               /* ÊéßÂà∂Â∑¶Âè≥ÊªöÂä® */
 
 /*
- *	±ﬂΩÁ≤Œ ˝
+ *    ËæπÁïåÂèÇÊï∞
  */
 typedef struct ROLLER_BORDER
 {
-	int16_t lsize;			/* –°‘≤Ω«≥ﬂ¥Á */
-	int16_t msize;			/* ¥Û‘≤Ω«≥ﬂ¥Á */
-	int16_t tspace;			/* –°‘≤Ω«µΩ∂•æ‡¿Î */
-	int16_t xspace;			/* ¥Û–°‘≤Ω«º‰º‰œ∂ (‘ –ÌŒ™∏∫£¨µ˛º”)*/
+    int16_t lsize;                      /* Â∞èÂúÜËßíÂ∞∫ÂØ∏ */
+    int16_t msize;                      /* Â§ßÂúÜËßíÂ∞∫ÂØ∏ */
+    int16_t tspace;                     /* Â∞èÂúÜËßíÂà∞È°∂Ë∑ùÁ¶ª */
+    int16_t xspace;                     /* Â§ßÂ∞èÂúÜËßíÈó¥Èó¥Èöô (ÂÖÅËÆ∏‰∏∫Ë¥üÔºåÂè†Âä†)*/
 	
-	int16_t x1;				/* »˝∏ˆ‘≤Ω«æÿ–Œ∂‘”¶µƒx◊¯±Í */
-	int16_t x2;
-	int16_t x3;
+    int16_t x1;                         /* ‰∏â‰∏™ÂúÜËßíÁü©ÂΩ¢ÂØπÂ∫îÁöÑxÂùêÊ†á */
+    int16_t x2;
+    int16_t x3;
 	
-	float ratio_sml;			/* ≥ﬂ¥Á◊Û÷–œµ ˝ */
-	float ratio_smr;			/* ≥ﬂ¥Á”“÷–œµ ˝ */
-	float ratio_aml;			/* Õ∏√˜∂»±‰ªª */
-	float ratio_amr;	
+    float ratio_sml;                    /* Â∞∫ÂØ∏Â∑¶‰∏≠Á≥ªÊï∞ */
+    float ratio_smr;                    /* Â∞∫ÂØ∏Âè≥‰∏≠Á≥ªÊï∞ */
+    float ratio_aml;                    /* ÈÄèÊòéÂ∫¶ÂèòÊç¢ */
+    float ratio_amr;	
 }ROLLER_BORDER;
 ROLLER_BORDER roller_border = {0};
 
-#define ROLLER_BORDER_LSIZE			roller_border.lsize
-#define ROLLER_BORDER_MSIZE			roller_border.msize
-#define ROLLER_BORDER_TSPACE		roller_border.tspace
-#define ROLLER_BORDER_XSPACE		roller_border.xspace
-#define ROLLER_BORDER_X1			roller_border.x1
-#define ROLLER_BORDER_X2			roller_border.x2
-#define ROLLER_BORDER_X3			roller_border.x3
-#define ROLLER_RATIO_SIZE_ML		roller_border.ratio_sml
-#define ROLLER_RATIO_SIZE_MR		roller_border.ratio_smr
-#define ROLLER_RATIO_ALPHA_ML		roller_border.ratio_aml
-#define ROLLER_RATIO_ALPHA_MR		roller_border.ratio_amr
+#define ROLLER_BORDER_LSIZE             roller_border.lsize
+#define ROLLER_BORDER_MSIZE             roller_border.msize
+#define ROLLER_BORDER_TSPACE            roller_border.tspace
+#define ROLLER_BORDER_XSPACE            roller_border.xspace
+#define ROLLER_BORDER_X1                roller_border.x1
+#define ROLLER_BORDER_X2                roller_border.x2
+#define ROLLER_BORDER_X3                roller_border.x3
+#define ROLLER_RATIO_SIZE_ML            roller_border.ratio_sml
+#define ROLLER_RATIO_SIZE_MR            roller_border.ratio_smr
+#define ROLLER_RATIO_ALPHA_ML           roller_border.ratio_aml
+#define ROLLER_RATIO_ALPHA_MR           roller_border.ratio_amr
 
 /*
- *	Àƒ∏ˆ‘≤Ω«æÿ–Œµƒ±‰ªª≤Œ ˝
+ *    Âõõ‰∏™ÂúÜËßíÁü©ÂΩ¢ÁöÑÂèòÊç¢ÂèÇÊï∞
  */
 typedef struct ROLLER_REGION
 {
-	float ix;
-	float iy;
-	uint8_t isize;
-	uint8_t Alpha;
+    float ix;
+    float iy;
+    uint8_t isize;
+    uint8_t Alpha;
 }ROLLER_REGION;
 ROLLER_REGION roller_region[4] = {0};
 
-#define	ROLLER_REGION_X(a)		roller_region[a].ix
-#define	ROLLER_REGION_Y(a)		roller_region[a].iy
-#define	ROLLER_REGION_SIZE(a)	roller_region[a].isize
-#define	ROLLER_REGION_ALPHA(a)	roller_region[a].Alpha
+#define	ROLLER_REGION_X(a)              roller_region[a].ix
+#define	ROLLER_REGION_Y(a)              roller_region[a].iy
+#define	ROLLER_REGION_SIZE(a)           roller_region[a].isize
+#define	ROLLER_REGION_ALPHA(a)          roller_region[a].Alpha
 
 
 
 /*
- *	ƒ⁄»›±‰ªª
+ *    ÂÜÖÂÆπÂèòÊç¢
  */
 static uint8_t index_buf[4] = {0xFF, 0xFF, 0xFF, 0xFF};
 static char data[] = {'a','b','c','d','e','f','g','h','i'};
@@ -80,187 +79,183 @@ static char data[] = {'a','b','c','d','e','f','g','h','i'};
 uint8_t index_cur = 0;
 
 /*
- *	∏¥Œª»˝∏ˆ‘≤Ω«æÿ–ŒµƒŒª÷√
+ *    Â§ç‰Ωç‰∏â‰∏™ÂúÜËßíÁü©ÂΩ¢ÁöÑ‰ΩçÁΩÆ
  */
 static void roller_reset(const arm_2d_region_t *ptRegion)
 {
-	ROLLER_REGION_X(0) = ROLLER_BORDER_X1 - ROLLER_BORDER_XSPACE - ROLLER_BORDER_LSIZE;
-	ROLLER_REGION_Y(0) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE;
-	ROLLER_REGION_SIZE(0) = ROLLER_BORDER_LSIZE;
-	ROLLER_REGION_ALPHA(0) = CONTROL_SCALE*CONTROL_SCALE_ALPHA;
+    ROLLER_REGION_X(0) = ROLLER_BORDER_X1 - ROLLER_BORDER_XSPACE - ROLLER_BORDER_LSIZE;
+    ROLLER_REGION_Y(0) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE;
+    ROLLER_REGION_SIZE(0) = ROLLER_BORDER_LSIZE;
+    ROLLER_REGION_ALPHA(0) = CONTROL_SCALE*CONTROL_SCALE_ALPHA;
 	
-	ROLLER_REGION_X(1) = ROLLER_BORDER_X1;
-	ROLLER_REGION_Y(1) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE;
-	ROLLER_REGION_SIZE(1) = ROLLER_BORDER_LSIZE;
-	ROLLER_REGION_ALPHA(1) = CONTROL_SCALE*CONTROL_SCALE_ALPHA;
+    ROLLER_REGION_X(1) = ROLLER_BORDER_X1;
+    ROLLER_REGION_Y(1) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE;
+    ROLLER_REGION_SIZE(1) = ROLLER_BORDER_LSIZE;
+    ROLLER_REGION_ALPHA(1) = CONTROL_SCALE*CONTROL_SCALE_ALPHA;
 	
-	ROLLER_REGION_X(2) = ROLLER_BORDER_X2;
-	ROLLER_REGION_Y(2) = ptRegion->tLocation.iY;
-	ROLLER_REGION_SIZE(2) = ROLLER_BORDER_MSIZE;
-	ROLLER_REGION_ALPHA(2) = CONTROL_SCALE_ALPHA;
+    ROLLER_REGION_X(2) = ROLLER_BORDER_X2;
+    ROLLER_REGION_Y(2) = ptRegion->tLocation.iY;
+    ROLLER_REGION_SIZE(2) = ROLLER_BORDER_MSIZE;
+    ROLLER_REGION_ALPHA(2) = CONTROL_SCALE_ALPHA;
 	
-	ROLLER_REGION_X(3) = ROLLER_BORDER_X3;
-	ROLLER_REGION_Y(3) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE;
-	ROLLER_REGION_SIZE(3) = ROLLER_BORDER_LSIZE;
-	ROLLER_REGION_ALPHA(3) = CONTROL_SCALE*CONTROL_SCALE_ALPHA;
-
+    ROLLER_REGION_X(3) = ROLLER_BORDER_X3;
+    ROLLER_REGION_Y(3) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE;
+    ROLLER_REGION_SIZE(3) = ROLLER_BORDER_LSIZE;
+    ROLLER_REGION_ALPHA(3) = CONTROL_SCALE*CONTROL_SCALE_ALPHA;
 }
 
 void roller_list_draw(	const arm_2d_tile_t *ptTarget, 
-						const arm_2d_region_t *ptRegion,
-						bool bIsNewFrame)
-{	
-	assert(ptRegion->tSize.iWidth >= ptRegion->tSize.iHeight*2);			/* »∑±£øÌŒ™¡Ω±∂∏ﬂ */
+                        const arm_2d_region_t *ptRegion,
+                        bool bIsNewFrame)
+{
+    assert(ptRegion->tSize.iWidth >= ptRegion->tSize.iHeight*2);                                                    /* Á°Æ‰øùÂÆΩ‰∏∫‰∏§ÂÄçÈ´ò */
 	
-	if(bIsNewFrame)
-	{
-		static uint8_t dtsize = 0;											/* ”√”⁄≥ﬂ¥ÁΩ•±‰ */
+    if(bIsNewFrame)
+    {
+        static uint8_t dtsize = 0;                                                                                  /* Áî®‰∫éÂ∞∫ÂØ∏Ê∏êÂèò */
 		
-		if(ROLLER_REGION_X(2) == 0){										/* µ⁄“ª¥ŒŒﬁ ˝æ› ±ÃÓ≥‰ */
-			/* ≥ﬂ¥Á≤Œ ˝ */
-			ROLLER_BORDER_MSIZE = ptRegion->tSize.iHeight;
-			ROLLER_BORDER_LSIZE = CONTROL_SCALE * ROLLER_BORDER_MSIZE;
-			ROLLER_BORDER_TSPACE = (ROLLER_BORDER_MSIZE-ROLLER_BORDER_LSIZE)>>1;
-			ROLLER_BORDER_XSPACE = (ptRegion->tSize.iWidth-ROLLER_BORDER_MSIZE-2*ROLLER_BORDER_LSIZE)>>2;
-			/* ◊¯±Í≤Œ ˝ */
-			ROLLER_BORDER_X1 = ptRegion->tLocation.iX + ROLLER_BORDER_XSPACE;
-			ROLLER_BORDER_X2 = ptRegion->tLocation.iX + 2*ROLLER_BORDER_XSPACE + ROLLER_BORDER_LSIZE;
-			ROLLER_BORDER_X3 = ptRegion->tLocation.iX + 3*ROLLER_BORDER_XSPACE + ROLLER_BORDER_LSIZE + ROLLER_BORDER_MSIZE;
-			/* ±‰ªªœµ ˝ */
-			ROLLER_RATIO_SIZE_ML = ROLLER_BORDER_TSPACE*1.0/(ROLLER_BORDER_LSIZE+ROLLER_BORDER_XSPACE);	/* ∏ﬂ/øÌ */
-			ROLLER_RATIO_SIZE_MR = ROLLER_BORDER_TSPACE*1.0/(ROLLER_BORDER_MSIZE+ROLLER_BORDER_XSPACE);
-			ROLLER_RATIO_ALPHA_ML = (1.0-CONTROL_SCALE)*CONTROL_SCALE_ALPHA*1.0/(ROLLER_BORDER_LSIZE+ROLLER_BORDER_XSPACE);
-			ROLLER_RATIO_ALPHA_MR = (1.0-CONTROL_SCALE)*CONTROL_SCALE_ALPHA*1.0/(ROLLER_BORDER_MSIZE+ROLLER_BORDER_XSPACE)*1.0;
+        if(ROLLER_REGION_X(2) == 0){                                                                                /* Á¨¨‰∏ÄÊ¨°Êó†Êï∞ÊçÆÊó∂Â°´ÂÖÖ */
+         /* Â∞∫ÂØ∏ÂèÇÊï∞ */
+            ROLLER_BORDER_MSIZE = ptRegion->tSize.iHeight;
+            ROLLER_BORDER_LSIZE = CONTROL_SCALE * ROLLER_BORDER_MSIZE;
+            ROLLER_BORDER_TSPACE = (ROLLER_BORDER_MSIZE-ROLLER_BORDER_LSIZE)>>1;
+            ROLLER_BORDER_XSPACE = (ptRegion->tSize.iWidth-ROLLER_BORDER_MSIZE-2*ROLLER_BORDER_LSIZE)>>2;
+            /* ÂùêÊ†áÂèÇÊï∞ */
+            ROLLER_BORDER_X1 = ptRegion->tLocation.iX + ROLLER_BORDER_XSPACE;
+            ROLLER_BORDER_X2 = ptRegion->tLocation.iX + 2*ROLLER_BORDER_XSPACE + ROLLER_BORDER_LSIZE;
+            ROLLER_BORDER_X3 = ptRegion->tLocation.iX + 3*ROLLER_BORDER_XSPACE + ROLLER_BORDER_LSIZE + ROLLER_BORDER_MSIZE;
+            /* ÂèòÊç¢Á≥ªÊï∞ */
+            ROLLER_RATIO_SIZE_ML = ROLLER_BORDER_TSPACE*1.0/(ROLLER_BORDER_LSIZE+ROLLER_BORDER_XSPACE);	/* È´ò/ÂÆΩ */
+            ROLLER_RATIO_SIZE_MR = ROLLER_BORDER_TSPACE*1.0/(ROLLER_BORDER_MSIZE+ROLLER_BORDER_XSPACE);
+            ROLLER_RATIO_ALPHA_ML = (1.0-CONTROL_SCALE)*CONTROL_SCALE_ALPHA*1.0/(ROLLER_BORDER_LSIZE+ROLLER_BORDER_XSPACE);
+            ROLLER_RATIO_ALPHA_MR = (1.0-CONTROL_SCALE)*CONTROL_SCALE_ALPHA*1.0/(ROLLER_BORDER_MSIZE+ROLLER_BORDER_XSPACE)*1.0;
 
-			roller_reset(ptRegion);
-		}
+            roller_reset(ptRegion);
+        }
 			
-		switch(roller_switch)
-		{
-			case -1:
-				/* ◊Û->”“ ∂º“‘xŒ™≤Œøº */
-				ROLLER_REGION_X(1) = ROLLER_REGION_X(1)+SPEED<ROLLER_BORDER_X2 ? ROLLER_REGION_X(1)+SPEED: ROLLER_BORDER_X2;
-				ROLLER_REGION_Y(1) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE - (ROLLER_REGION_X(1)-ROLLER_BORDER_X1)*ROLLER_RATIO_SIZE_ML;
-				ROLLER_REGION_SIZE(1) = ROLLER_BORDER_LSIZE+(ROLLER_REGION_X(1)-ROLLER_BORDER_X1)*ROLLER_RATIO_SIZE_ML*2;
-				ROLLER_REGION_ALPHA(1) = CONTROL_SCALE*CONTROL_SCALE_ALPHA + (ROLLER_REGION_X(1)-ROLLER_BORDER_X1)*ROLLER_RATIO_ALPHA_ML;
+        switch(roller_switch)
+        {
+            case -1:
+            /* Â∑¶->Âè≥ ÈÉΩ‰ª•x‰∏∫ÂèÇËÄÉ */
+                ROLLER_REGION_X(1) = ROLLER_REGION_X(1)+SPEED<ROLLER_BORDER_X2 ? ROLLER_REGION_X(1)+SPEED: ROLLER_BORDER_X2;
+                ROLLER_REGION_Y(1) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE - (ROLLER_REGION_X(1)-ROLLER_BORDER_X1)*ROLLER_RATIO_SIZE_ML;
+                ROLLER_REGION_SIZE(1) = ROLLER_BORDER_LSIZE+(ROLLER_REGION_X(1)-ROLLER_BORDER_X1)*ROLLER_RATIO_SIZE_ML*2;
+                ROLLER_REGION_ALPHA(1) = CONTROL_SCALE*CONTROL_SCALE_ALPHA + (ROLLER_REGION_X(1)-ROLLER_BORDER_X1)*ROLLER_RATIO_ALPHA_ML;
 			
-				ROLLER_REGION_X(2) = ROLLER_REGION_X(1)+ROLLER_REGION_SIZE(1)+ROLLER_BORDER_XSPACE;
-				ROLLER_REGION_Y(2) = ptRegion->tLocation.iY + (ROLLER_REGION_X(2)-ROLLER_BORDER_X2)*ROLLER_RATIO_SIZE_MR;
-				ROLLER_REGION_SIZE(2) = ROLLER_BORDER_MSIZE-(ROLLER_REGION_X(2)-ROLLER_BORDER_X2)*ROLLER_RATIO_SIZE_MR*2;
-				ROLLER_REGION_ALPHA(2) = CONTROL_SCALE_ALPHA-(ROLLER_REGION_X(2)-ROLLER_BORDER_X2)*ROLLER_RATIO_ALPHA_MR;
+                ROLLER_REGION_X(2) = ROLLER_REGION_X(1)+ROLLER_REGION_SIZE(1)+ROLLER_BORDER_XSPACE;
+                ROLLER_REGION_Y(2) = ptRegion->tLocation.iY + (ROLLER_REGION_X(2)-ROLLER_BORDER_X2)*ROLLER_RATIO_SIZE_MR;
+                ROLLER_REGION_SIZE(2) = ROLLER_BORDER_MSIZE-(ROLLER_REGION_X(2)-ROLLER_BORDER_X2)*ROLLER_RATIO_SIZE_MR*2;
+                ROLLER_REGION_ALPHA(2) = CONTROL_SCALE_ALPHA-(ROLLER_REGION_X(2)-ROLLER_BORDER_X2)*ROLLER_RATIO_ALPHA_MR;
 
-				ROLLER_REGION_X(3) = ROLLER_REGION_X(2) + ROLLER_REGION_SIZE(2) + ROLLER_BORDER_XSPACE;
-				ROLLER_REGION_X(0) = ROLLER_REGION_X(1) - ROLLER_BORDER_LSIZE - ROLLER_BORDER_XSPACE;
+                ROLLER_REGION_X(3) = ROLLER_REGION_X(2) + ROLLER_REGION_SIZE(2) + ROLLER_BORDER_XSPACE;
+                ROLLER_REGION_X(0) = ROLLER_REGION_X(1) - ROLLER_BORDER_LSIZE - ROLLER_BORDER_XSPACE;
 			
-				if(ROLLER_REGION_X(1) == ROLLER_BORDER_X2){			
-					roller_reset(ptRegion);
+                if(ROLLER_REGION_X(1) == ROLLER_BORDER_X2){			
+                    roller_reset(ptRegion);
 					
-					roller_switch = 0;
-					index_cur = index_cur>0 ? index_cur-1 : DATA_SIZE-1;
-				}
-
-			break;
+                    roller_switch = 0;
+                    index_cur = index_cur>0 ? index_cur-1 : DATA_SIZE-1;
+                }
+            break;
 				
-			case 1:
-				/* ◊Û<-”“ */
-				ROLLER_REGION_X(2) = ROLLER_REGION_X(2)-SPEED>ROLLER_BORDER_X1 ? ROLLER_REGION_X(2)-SPEED: ROLLER_BORDER_X1;
-				ROLLER_REGION_Y(2) = ptRegion->tLocation.iY + (ROLLER_BORDER_X2-ROLLER_REGION_X(2))*ROLLER_RATIO_SIZE_ML;
-				ROLLER_REGION_SIZE(2) = ROLLER_BORDER_MSIZE-(ROLLER_BORDER_X2-ROLLER_REGION_X(2))*ROLLER_RATIO_SIZE_ML*2;
-				ROLLER_REGION_ALPHA(2) = CONTROL_SCALE_ALPHA - (ROLLER_BORDER_X2-ROLLER_REGION_X(2))*ROLLER_RATIO_ALPHA_ML;
+            case 1:
+                /* Â∑¶<-Âè≥ */
+                ROLLER_REGION_X(2) = ROLLER_REGION_X(2)-SPEED>ROLLER_BORDER_X1 ? ROLLER_REGION_X(2)-SPEED: ROLLER_BORDER_X1;
+                ROLLER_REGION_Y(2) = ptRegion->tLocation.iY + (ROLLER_BORDER_X2-ROLLER_REGION_X(2))*ROLLER_RATIO_SIZE_ML;
+                ROLLER_REGION_SIZE(2) = ROLLER_BORDER_MSIZE-(ROLLER_BORDER_X2-ROLLER_REGION_X(2))*ROLLER_RATIO_SIZE_ML*2;
+                ROLLER_REGION_ALPHA(2) = CONTROL_SCALE_ALPHA - (ROLLER_BORDER_X2-ROLLER_REGION_X(2))*ROLLER_RATIO_ALPHA_ML;
 			
-				ROLLER_REGION_X(3) = ROLLER_REGION_X(2)+ROLLER_REGION_SIZE(2)+ROLLER_BORDER_XSPACE;
-				ROLLER_REGION_Y(3) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE - (ROLLER_BORDER_X3-ROLLER_REGION_X(3))*ROLLER_RATIO_SIZE_MR;
-				ROLLER_REGION_SIZE(3) = ROLLER_BORDER_LSIZE+(ROLLER_BORDER_X3-ROLLER_REGION_X(3))*ROLLER_RATIO_SIZE_MR*2;
-				ROLLER_REGION_ALPHA(3) = CONTROL_SCALE_ALPHA*CONTROL_SCALE+(ROLLER_BORDER_X3-ROLLER_REGION_X(3))*ROLLER_RATIO_ALPHA_MR;
+                ROLLER_REGION_X(3) = ROLLER_REGION_X(2)+ROLLER_REGION_SIZE(2)+ROLLER_BORDER_XSPACE;
+                ROLLER_REGION_Y(3) = ptRegion->tLocation.iY + ROLLER_BORDER_TSPACE - (ROLLER_BORDER_X3-ROLLER_REGION_X(3))*ROLLER_RATIO_SIZE_MR;
+                ROLLER_REGION_SIZE(3) = ROLLER_BORDER_LSIZE+(ROLLER_BORDER_X3-ROLLER_REGION_X(3))*ROLLER_RATIO_SIZE_MR*2;
+                ROLLER_REGION_ALPHA(3) = CONTROL_SCALE_ALPHA*CONTROL_SCALE+(ROLLER_BORDER_X3-ROLLER_REGION_X(3))*ROLLER_RATIO_ALPHA_MR;
 			
-				ROLLER_REGION_X(1) = ROLLER_REGION_X(2) - ROLLER_BORDER_XSPACE - ROLLER_BORDER_LSIZE;
-				ROLLER_REGION_X(0) = ROLLER_REGION_X(3) + ROLLER_BORDER_XSPACE + ROLLER_REGION_SIZE(3);
+                ROLLER_REGION_X(1) = ROLLER_REGION_X(2) - ROLLER_BORDER_XSPACE - ROLLER_BORDER_LSIZE;
+                ROLLER_REGION_X(0) = ROLLER_REGION_X(3) + ROLLER_BORDER_XSPACE + ROLLER_REGION_SIZE(3);
 			
-				if(ROLLER_REGION_X(2) == ROLLER_BORDER_X1){			
-					roller_reset(ptRegion);
+                if(ROLLER_REGION_X(2) == ROLLER_BORDER_X1){			
+                    roller_reset(ptRegion);
 					
-					roller_switch = 0;
-					index_cur = index_cur<DATA_SIZE-1 ? index_cur+1 : 0;
-				}
-			break;
+                    roller_switch = 0;
+                    index_cur = index_cur<DATA_SIZE-1 ? index_cur+1 : 0;
+                }
+                break;
 					
-			default:
-				
-			break;
-		}
+                default:		
+                break;
+        }
 
-		/*
-		 *	º∆À„œ¬“ªÃıƒ⁄»›œ¬±Í
-		 */	
-		switch(roller_switch)
-		{
-			case -1:
-			/* ◊Û->”“ */
-				index_buf[0] = index_buf[1]>0 ? index_buf[1]-1 : DATA_SIZE-1;
-			break;
+        /*
+         *    ËÆ°ÁÆó‰∏ã‰∏ÄÊù°ÂÜÖÂÆπ‰∏ãÊ†á
+         */	
+        switch(roller_switch)
+        {
+            case -1:
+            /* Â∑¶->Âè≥ */
+                index_buf[0] = index_buf[1]>0 ? index_buf[1]-1 : DATA_SIZE-1;
+            break;
 			
-			case 1:
-			/* ◊Û<-”“ */
-				index_buf[0] = index_buf[3]+1<DATA_SIZE ? index_buf[3]+1 : 0;
-			break;
+            case 1:
+            /* Â∑¶<-Âè≥ */
+                index_buf[0] = index_buf[3]+1<DATA_SIZE ? index_buf[3]+1 : 0;
+            break;
 			
-			default:
-			break;
-		}
+            default:
+            break;
+        }
 		
-		if(index_buf[2] != index_cur && roller_switch==0)
-		{
-			/* “∆∂ØÕÍ≥… */
-			index_buf[1] = index_cur>0 ? index_cur-1 : DATA_SIZE-1;
-			index_buf[2] = index_cur;
-			index_buf[3] = index_cur+1<DATA_SIZE ? index_cur+1 : 0;
-		}
-	}
+        if(index_buf[2] != index_cur && roller_switch==0)
+        {
+            /* ÁßªÂä®ÂÆåÊàê */
+            index_buf[1] = index_cur>0 ? index_cur-1 : DATA_SIZE-1;
+            index_buf[2] = index_cur;
+            index_buf[3] = index_cur+1<DATA_SIZE ? index_cur+1 : 0;
+        }
+    }
 	
 	
+    uint8_t i=0;
+    if(roller_switch==0)
+        i=1;
+    else
+        i=0;
 	
-	uint8_t i=0;
-	if(roller_switch==0)
-		i=1;
-	else
-		i=0;
-	
-	for(;i<4;i++)
-	{
-		/* 
-		 *	‘≤Ω«æÿ–Œ
-		 */
-		draw_round_corner_box(  ptTarget,
-							(const arm_2d_region_t []) {{
-								  .tLocation = {
-									  .iX = roller_region[i].ix,
-									  .iY =	roller_region[i].iy,
-								  },
-								  .tSize = {
-									  .iWidth  = roller_region[i].isize,
-									  .iHeight = roller_region[i].isize,
-								  },
-							  }},
-							GLCD_COLOR_DARK_GREY,
-							roller_region[i].Alpha,
-							bIsNewFrame);
-		/*
-		 *	ƒ⁄»›
-		 */					  
-		arm_lcd_text_set_target_framebuffer((arm_2d_tile_t *)ptTarget);
-		arm_lcd_text_set_draw_region((arm_2d_region_t []) {{
-											  .tLocation = {
-												  .iX = ROLLER_REGION_X(i) + 10,
-												  .iY = ROLLER_REGION_Y(i) + 10,
-											  },
-											  .tSize = {
-												  .iWidth  = 60,
-												  .iHeight = 40,
-											  },
-										  }});
-		arm_lcd_text_set_colour(GLCD_COLOR_WHITE, GLCD_COLOR_WHITE);
-		arm_lcd_text_location(0,0);					  
-		arm_lcd_printf("%c\r\n",data[index_buf[i]]);
-	}	
+    for(;i<4;i++)
+    {
+        /* 
+         *    ÂúÜËßíÁü©ÂΩ¢
+         */
+        draw_round_corner_box( ptTarget,
+                               (const arm_2d_region_t []) {{
+                                   .tLocation = {
+                                   .iX = roller_region[i].ix,
+                                   .iY = roller_region[i].iy,
+                                },
+                               .tSize = {
+                                   .iWidth  = roller_region[i].isize,
+                                   .iHeight = roller_region[i].isize,
+                               },
+                               }},
+                               GLCD_COLOR_DARK_GREY,
+                               roller_region[i].Alpha,
+                               bIsNewFrame);
+        /*
+         *    ÂÜÖÂÆπ
+         */					  
+        arm_lcd_text_set_target_framebuffer((arm_2d_tile_t *)ptTarget);
+        arm_lcd_text_set_draw_region( (arm_2d_region_t []) {{
+                                      .tLocation = {
+                                          .iX = ROLLER_REGION_X(i) + 10,
+                                          .iY = ROLLER_REGION_Y(i) + 10,
+                                      },
+                                      .tSize = {
+                                          .iWidth  = 60,
+                                          .iHeight = 40,
+                                      },
+                                    }});
+        arm_lcd_text_set_colour(GLCD_COLOR_WHITE, GLCD_COLOR_WHITE);
+        arm_lcd_text_location(0,0);					  
+        arm_lcd_printf("%c\r\n",data[index_buf[i]]);
+    }	
 }
 
