b61258124b939363f16eadd27b6364436ea9e22f
Z_Tam <2403626628@qq.com>
2022-10-03 21:17:55 +0800

    Update roller_list.h
    删除tab

--- examples/common/controls/roller_list.h
+++ examples/common/controls/roller_list.h
@@ -8,9 +8,9 @@ extern int8_t roller_switch;
 #define ROLLER_LIST_DEC	if(roller_switch==0)roller_switch = -1;
 
 
-void roller_list_draw(	const arm_2d_tile_t *ptTarget, 
-						const arm_2d_region_t *ptRegion,
-						bool bIsNewFrame);
+void roller_list_draw(  const arm_2d_tile_t *ptTarget, 
+                        const arm_2d_region_t *ptRegion,
+                        bool bIsNewFrame);
 
 
 
