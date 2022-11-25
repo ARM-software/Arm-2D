d2c27215e851140f033f9e3cab6f129deb3a0712
Gabriel Wang <gabriel.wang@arm.com>
2022-11-25 22:19:59 +0000

    add GRAY8 display support to template

--- examples/[template][bare-metal][pfb]/main.c
+++ examples/[template][bare-metal][pfb]/main.c
@@ -61,12 +61,32 @@ int64_t arm_2d_helper_get_system_timestamp(void)
 
 __OVERRIDE_WEAK 
 void Disp0_DrawBitmap(  int16_t x, 
-                            int16_t y, 
-                            int16_t width, 
-                            int16_t height, 
-                            const uint8_t *bitmap)
+                        int16_t y, 
+                        int16_t width, 
+                        int16_t height, 
+                        const uint8_t *bitmap)
 {
-#if __GLCD_CFG_COLOUR_DEPTH__ == 32
+#if __DISP0_CFG_COLOUR_DEPTH__ == 8
+    extern
+    void __arm_2d_impl_gray8_to_rgb565( uint8_t *__RESTRICT pchSourceBase,
+                                        int16_t iSourceStride,
+                                        uint16_t *__RESTRICT phwTargetBase,
+                                        int16_t iTargetStride,
+                                        arm_2d_size_t *__RESTRICT ptCopySize);
+
+    static uint16_t s_hwFrameBuffer[BENCHMARK_PFB_BLOCK_WIDTH * BENCHMARK_PFB_BLOCK_HEIGHT];
+    
+    arm_2d_size_t size = {
+        .iWidth = width,
+        .iHeight = height,
+    };
+    __arm_2d_impl_gray8_to_rgb565( (uint8_t *)bitmap,
+                                    width,
+                                    (uint16_t *)s_hwFrameBuffer,
+                                    width,
+                                    &size);
+    GLCD_DrawBitmap(x, y, width, height, (const uint8_t *)s_hwFrameBuffer);
+#elif __DISP0_CFG_COLOUR_DEPTH__ == 32
     extern
     void __arm_2d_impl_cccn888_to_rgb565(uint32_t *__RESTRICT pwSourceBase,
                                         int16_t iSourceStride,
@@ -83,9 +103,10 @@ void Disp0_DrawBitmap(  int16_t x,
                                     (uint16_t *)bitmap,
                                     width,
                                     &size);
-#endif
-
     GLCD_DrawBitmap(x, y, width, height, bitmap);
+#else
+    GLCD_DrawBitmap(x, y, width, height, bitmap);
+#endif
 }
 
 /*----------------------------------------------------------------------------
