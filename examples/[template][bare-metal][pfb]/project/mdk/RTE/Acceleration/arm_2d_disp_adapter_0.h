d68d2d41bb30bab4ee322c83fb406f35a1deaffb
Gabriel Wang <gabriel.wang@arm.com>
2023-02-07 12:07:12 +0000

    Update arm_2d_disp_adapter_0.h

--- examples/[template][bare-metal][pfb]/project/mdk/RTE/Acceleration/arm_2d_disp_adapter_0.h
+++ examples/[template][bare-metal][pfb]/project/mdk/RTE/Acceleration/arm_2d_disp_adapter_0.h
@@ -106,7 +106,7 @@ extern "C" {
 // <q>Disable the navigation layer
 // <i> Remove the navigation layer for this display adapter. NOTE: Disable the navigation layer will also remove the real-time FPS display.
 #ifndef __DISP0_CFG_DISABLE_NAVIGATION_LAYER__
-#   define __DISP0_CFG_DISABLE_NAVIGATION_LAYER__  1
+#   define __DISP0_CFG_DISABLE_NAVIGATION_LAYER__  0
 #endif
 
 // <q>Enable the virtual resource helper service
