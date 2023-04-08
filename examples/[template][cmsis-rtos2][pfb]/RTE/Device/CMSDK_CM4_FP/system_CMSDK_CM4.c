64bff1aa414ae241477834fb1f0b1cb6b001c8e1
Gabriel Wang <gabriel.wang@arm.com>
2023-04-08 13:38:22 +0100

    fix project for Cortex-M4F

--- examples/[template][cmsis-rtos2][pfb]/RTE/Device/CMSDK_CM4_FP/system_CMSDK_CM4.c
+++ examples/[template][cmsis-rtos2][pfb]/RTE/Device/CMSDK_CM4_FP/system_CMSDK_CM4.c
@@ -1,11 +1,8 @@
-/**************************************************************************//**
+/******************************************************************************
  * @file     system_CMSDK_CM4.c
- * @brief    CMSIS Device System Source File for
- *           CMSDK_M4 Device
- * @version  V4.00
- * @date     02. November 2015
+ * @brief    CMSIS System Source File for CMSDK_M4 Device
  ******************************************************************************/
-/* Copyright (c) 2011 - 2015 ARM LIMITED
+/* Copyright (c) 2011 - 2022 ARM LIMITED
 
    All rights reserved.
    Redistribution and use in source and binary forms, with or without
@@ -32,40 +29,53 @@
    POSSIBILITY OF SUCH DAMAGE.
    ---------------------------------------------------------------------------*/
 
-
-#if   defined (CMSDK_CM4)
+#if   defined (CMSDK_CM4)    || defined (CMSDK_CM4_VHT)
   #include "CMSDK_CM4.h"
-#elif defined (CMSDK_CM4_FP)
+#elif defined (CMSDK_CM4_FP) || defined (CMSDK_CM4_FP_VHT)
   #include "CMSDK_CM4_FP.h"
 #else
   #error device not specified!
 #endif
+
 
 /*----------------------------------------------------------------------------
   Define clocks
  *----------------------------------------------------------------------------*/
 #define  XTAL            (50000000UL)     /* Oscillator frequency */
 
-#define  SYSTEM_CLOCK    (XTAL / 2)
+#define  SYSTEM_CLOCK    (XTAL / 2U)
 
+/*----------------------------------------------------------------------------
+  Exception / Interrupt Vector table
+ *----------------------------------------------------------------------------*/
+extern const VECTOR_TABLE_Type __VECTOR_TABLE[256];
 
 /*----------------------------------------------------------------------------
   System Core Clock Variable
  *----------------------------------------------------------------------------*/
 uint32_t SystemCoreClock = SYSTEM_CLOCK;  /* System Core Clock Frequency */
 
-
+/*----------------------------------------------------------------------------
+  System Core Clock update function
+ *----------------------------------------------------------------------------*/
 void SystemCoreClockUpdate (void)
 {
   SystemCoreClock = SYSTEM_CLOCK;
 }
 
+/*----------------------------------------------------------------------------
+  System initialization function
+ *----------------------------------------------------------------------------*/
 void SystemInit (void)
 {
-  #if (__FPU_USED == 1)
-    SCB->CPACR |= ((3UL << 10*2) |                 /* set CP10 Full Access */
-                   (3UL << 11*2)  );               /* set CP11 Full Access */
-  #endif
+#if defined (__VTOR_PRESENT) && (__VTOR_PRESENT == 1U)
+  SCB->VTOR = (uint32_t) &(__VECTOR_TABLE[0]);
+#endif
+
+#if defined (__FPU_USED) && (__FPU_USED == 1U)
+  SCB->CPACR |= ((3U << 10U*2U) |           /* enable CP10 Full Access */
+                 (3U << 11U*2U)  );         /* enable CP11 Full Access */
+#endif
 
 #ifdef UNALIGNED_SUPPORT_DISABLE
   SCB->CCR |= SCB_CCR_UNALIGN_TRP_Msk;
