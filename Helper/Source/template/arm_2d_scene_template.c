2c53988613fb6bfba1631100d1be8dc0ae7843a9
Gabriel Wang <gabriel.wang@arm.com>
2023-05-30 13:17:29 +0800

    Update arm_2d_scene_template.c
    remove background handler

--- Helper/Source/template/arm_2d_scene_template.c
+++ Helper/Source/template/arm_2d_scene_template.c
@@ -151,22 +151,6 @@ static void __before_scene_<name>_switching_out(arm_2d_scene_t *ptScene)
 }
 
 static
-IMPL_PFB_ON_DRAW(__pfb_draw_scene_<name>_background_handler)
-{
-    user_scene_<name>_t *ptThis = (user_scene_<name>_t *)pTarget;
-    ARM_2D_UNUSED(ptTile);
-    ARM_2D_UNUSED(bIsNewFrame);
-    /*-----------------------draw back ground begin-----------------------*/
-
-
-
-    /*-----------------------draw back ground end  -----------------------*/
-    arm_2d_op_wait_async(NULL);
-
-    return arm_fsm_rt_cpl;
-}
-
-static
 IMPL_PFB_ON_DRAW(__pfb_draw_scene_<name>_handler)
 {
     user_scene_<name>_t *ptThis = (user_scene_<name>_t *)pTarget;
@@ -297,7 +281,6 @@ user_scene_<name>_t *__arm_2d_scene_<name>_init(   arm_2d_scene_player_t *ptDisp
         .use_as__arm_2d_scene_t = {
             /* Please uncommon the callbacks if you need them
              */
-            //.fnBackground   = &__pfb_draw_scene_<name>_background_handler,
             .fnScene        = &__pfb_draw_scene_<name>_handler,
             .ptDirtyRegion  = (arm_2d_region_list_item_t *)s_tDirtyRegions,
             
