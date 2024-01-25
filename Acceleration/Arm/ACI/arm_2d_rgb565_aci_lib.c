430333bfa1ce4da4e8c19108bc73312de72167aa
FabKlein <fabien.klein@arm.com>
2024-01-25 14:10:35 +0000

    increased heap, added CDE + display test, CDE vs Helium compare test (bis)

--- Acceleration/Arm/ACI/arm_2d_rgb565_aci_lib.c
+++ Acceleration/Arm/ACI/arm_2d_rgb565_aci_lib.c
@@ -35,54 +35,122 @@
 #include "arm_2d_rgb565_aci_lib.h"
 
 
-void __arm_2d_aci_init(void)
-{
-    uint32_t        coproPrivMask = 0;
-    /* Enable Full access privileges for coprocessor IDs deduced from compiler exported bitmap mask __ARM_FEATURE_CDE_COPROC */
-    /* This library requires +cdecp0 compilation, enabling bit 0 */
+#if COMPARE_CDE_TEST
+#include <stdio.h>
 
+#define DUMP_Q15_VEC(vec)   printf("%s { %d %d %d %d %d %d %d %d }\n",#vec, vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6], vec[7])
+
+int COMPARE_Q15_VEC(uint16x8_t vec1, uint16x8_t vec2)
+{
+    for (int i = 0; i < 8; i++) {
+        if (vec1[i] != vec2[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+#endif
+
+static void __arm_2d_cde_rgb565_init(void)
+{
 #define COPRO_FULL_ACCESS 0x3U
 
-    for (uint32_t i = 0; i < 7; i++) {
-        if (__ARM_FEATURE_CDE_COPROC & (1 << i))
-            coproPrivMask |= (COPRO_FULL_ACCESS << (i * 2));
-    }
-
-    SCB->CPACR |= coproPrivMask;
+    SCB->CPACR |= COPRO_FULL_ACCESS << (ARM_2D_RGB565_ACI_LIB_COPRO_ID * 2);
     __ISB();
 }
 
 
 __OVERRIDE_WEAK
-void __arm_2d_impl_rgb565_colour_filling_with_opacity(uint16_t *
-                                                      __restrict pTargetBase,
-                                                      int16_t iTargetStride,
-                                                      arm_2d_size_t *
-                                                      __restrict ptCopySize,
-                                                      uint16_t Colour,
-                                                      uint_fast16_t hwRatio)
+    void __arm_2d_impl_rgb565_colour_filling_with_opacity(uint16_t *
+                                                          __restrict pTargetBase,
+                                                          int16_t iTargetStride,
+                                                          arm_2d_size_t *
+                                                          __restrict ptCopySize,
+                                                          uint16_t Colour, uint_fast16_t hwRatio)
 {
 
 #if !defined(__ARM_2D_CFG_UNSAFE_IGNORE_ALPHA_255_COMPENSATION__)
     hwRatio += (hwRatio == 255);
 #endif
     uint16x8_t      vecColour = vdupq_n_u16(Colour);
-    uint16x8_t      vecRatio = vdupq_n_u16((uint16_t)(hwRatio >> 1));
+    uint16x8_t      vecRatio = vdupq_n_u16((uint16_t) (hwRatio >> 0));
     int32_t         blkCnt;
+
+#if COMPARE_CDE_TEST
+    uint16_t        ratio1x8 = hwRatio * 8;
+    uint16_t        ratio1x4 = hwRatio * 4;
+    uint16_t        ratio2x8 = (256 - hwRatio) * 8;
+    uint16_t        ratio2x4 = (256 - hwRatio) * 4;
+
+    uint16x8_t      vecMaskR = vdupq_n_u16(0x001f);
+    uint16x8_t      vecMaskG = vdupq_n_u16(0x003f);
+    uint16x8_t      vecMaskBpck = vdupq_n_u16(0x00f8);
+    uint16x8_t      vecMaskGpck = vdupq_n_u16(0x00fc);
+    uint16x8_t      vecIn;
+    uint16x8_t      vecColorR, vecColorB, vecColorG;
+
+    /* unpack color & scale */
+    vecIn = vdupq_n_u16(Colour);
+    vecColorR = (vecIn & vecMaskR) * ratio1x8;
+
+    vecColorB = (vecIn >> 11) * ratio1x8;
+
+    vecColorG = vecIn >> 5;
+    vecColorG = (vecColorG & vecMaskG) * ratio1x4;
+#endif
 
     for (int32_t y = 0; y < ptCopySize->iHeight; y++) {
         uint16_t       *phwTarget = pTargetBase;
 
         blkCnt = ptCopySize->iWidth;
         do {
-            mve_pred16_t    p = vctp16q((uint32_t)blkCnt);
+            mve_pred16_t    p = vctp16q((uint32_t) blkCnt);
 
+#if COMPARE_CDE_TEST
+            uint16x8_t      vecR0, vecB0, vecG0;
+            uint16x8_t      vecR1, vecB1, vecG1;
+
+            /* unpack stream */
+            vecIn = vld1q(phwTarget);
+            vecR1 = vecIn & vecMaskR;
+
+            vecB1 = vecIn >> 11;
+
+            vecG1 = vecIn >> 5;
+            vecG1 = vecG1 & vecMaskG;
+
+
+            /* merge */
+            vecR0 = vecColorR + vecR1 * ratio2x8;
+            vecR0 = vecR0 >> 8;
+
+            vecG0 = vecColorG + vecG1 * ratio2x4;
+            vecG0 = vecG0 >> 8;
+
+            vecB0 = vecColorB + vecB1 * ratio2x8;
+            vecB0 = vecB0 >> 8;
+
+            /* pack */
+            uint16x8_t      vOut = vecR0 >> 3 | vmulq((vecG0 & vecMaskGpck), 8)
+                | vmulq((vecB0 & vecMaskBpck), 256);
+
+
+            uint16x8_t      cde = __arm_2d_cde_vrgb565_blend(vecColour,
+                                                             vecIn,
+
+                                                             vecRatio);
+
+            if (COMPARE_Q15_VEC(cde, vOut) == 0) {
+                printf("\nhwRatio %d\n", hwRatio);
+                DUMP_Q15_VEC(vecIn);
+                DUMP_Q15_VEC(vecColour);
+                DUMP_Q15_VEC(cde);
+                DUMP_Q15_VEC(vOut);
+            }
+#endif
             vst1q_p_u16(phwTarget,
-                        __arm_2d_cde_vrgb565_blend_m(
-                            vld1q_z(phwTarget, p),
-                            vecColour,
-                            vecRatio, p),
-                        p);
+                        __arm_2d_cde_vrgb565_blend_m(vld1q_z(phwTarget, p),
+                                                     vecColour, vecRatio, p), p);
 
             phwTarget += 8;
             blkCnt -= 8;
@@ -93,3 +161,9 @@ void __arm_2d_impl_rgb565_colour_filling_with_opacity(uint16_t *
     }
 }
 #endif
+
+void __arm_2d_aci_init(void)
+{
+    if (__ARM_FEATURE_CDE_COPROC & (1 << ARM_2D_RGB565_ACI_LIB_COPRO_ID))
+        __arm_2d_cde_rgb565_init();
+}
