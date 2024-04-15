cf3fd42122686c4d7b6c6a37a2158ceaec27a244
Gabriel Wang <gabriel.wang@arm.com>
2024-04-15 11:30:02 +0100

    Update __arm_2d_helper_common.h

--- Helper/Include/__arm_2d_helper_common.h
+++ Helper/Include/__arm_2d_helper_common.h
@@ -696,7 +696,7 @@ extern "C" {
  * 
  */
 #define __impl_fb(__name, __width, __height, __type, ...)                       \
-            ARM_NOINIT __type                                                   \
+            __attribute__((section(".bss.noinit." #__name))) __type             \
                 __name##Buffer[(__width) * (__height)];                         \
             const arm_2d_tile_t __name = {                                      \
                 .tRegion = {                                                    \
