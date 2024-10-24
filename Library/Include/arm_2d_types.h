3a8ae7a9b485f1d96d9ea6b3b30d6d18d9ed8733
Gabriel Wang <gabriel.wang@arm.com>
2024-10-24 23:14:31 +0100

    fix typo

--- Library/Include/arm_2d_types.h
+++ Library/Include/arm_2d_types.h
@@ -21,8 +21,8 @@
  * Title:        arm_2d_types.h
  * Description:  Public header file to contain the Arm-2D structs
  *
- * $Date:        29. Aug 2024
- * $Revision:    V.1.2.15
+ * $Date:        24. Oct 2024
+ * $Revision:    V.1.2.16
  *
  * Target Processor:  Cortex-M cores
  * -------------------------------------------------------------------- */
@@ -676,7 +676,7 @@ typedef enum {
     ARM_2D_ALIGN_BOTTOM_MIDDLE      = ARM_2D_ALIGN_BOTTOM                       /*!< align to the middle of the bottom */
                                     | ARM_2D_ALIGN_LEFT
                                     | ARM_2D_ALIGN_RIGHT,
-    ARM_2D_ALIGN_BOTTOM_CENTRE      = ARM_2D_ALIGN_TOP_MIDDLE,
+    ARM_2D_ALIGN_BOTTOM_CENTRE      = ARM_2D_ALIGN_BOTTOM_MIDDLE,
 
     ARM_2D_ALIGN_MIDDLE_LEFT        = ARM_2D_ALIGN_LEFT                         /*!< align to the middle of the left side */
                                     | ARM_2D_ALIGN_BOTTOM
