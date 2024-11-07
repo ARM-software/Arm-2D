8795fceee100b8b29ea3ab4e11d2350fa26388e2
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2024-11-07 02:59:46 +0000

    Update arm_2d_cfg.h

--- Library/Include/template/arm_2d_cfg.h
+++ Library/Include/template/arm_2d_cfg.h
@@ -63,7 +63,7 @@ extern "C" {
 // <i> Note that enabling this feature will add the support for a special colour type: ARM_2D_CHANNEL_8in32
 // <i> This feature is disabled by default to save code size
 #ifndef __ARM_2D_CFG_SUPPORT_COLOUR_CHANNEL_ACCESS__
-#   define __ARM_2D_CFG_SUPPORT_COLOUR_CHANNEL_ACCESS__             1
+#   define __ARM_2D_CFG_SUPPORT_COLOUR_CHANNEL_ACCESS__             0
 #endif
 
 // <q>Enable ccca8888(ARGB8888) implicit conversion 
