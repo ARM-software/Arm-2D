0439619adc7f07b085a7b292250a58a9a9e7f89f
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-04-14 06:15:53 +0100

    Update arm_2d_scene_rickrolling.c

--- examples/demos/arm_2d_scene_rickrolling.c
+++ examples/demos/arm_2d_scene_rickrolling.c
@@ -210,16 +210,6 @@ IMPL_PFB_ON_DRAW(__pfb_draw_scene_rickrolling_handler)
                     &__centre_region);
                                     
         }
-
-        /* draw text at the top-left corner */
-
-        arm_lcd_text_set_target_framebuffer((arm_2d_tile_t *)ptTile);
-        arm_lcd_text_set_font(&ARM_2D_FONT_6x8.use_as__arm_2d_font_t);
-        arm_lcd_text_set_draw_region(NULL);
-        arm_lcd_text_set_colour(GLCD_COLOR_RED, GLCD_COLOR_WHITE);
-        arm_lcd_text_location(0,0);
-        arm_lcd_puts("Scene rickrolling");
-
     /*-----------------------draw the foreground end  -----------------------*/
     }
     ARM_2D_OP_WAIT_ASYNC();
