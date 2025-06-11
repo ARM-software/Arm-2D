4d13e17ef08c81bf281f03953320d3fae8d2e56b
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-06-11 09:30:42 +0100

    Update tjpgd.h

--- examples/common/loader/tjpgd_loader/tjpgd/tjpgd.h
+++ examples/common/loader/tjpgd_loader/tjpgd/tjpgd.h
@@ -16,8 +16,8 @@ extern "C" {
 typedef unsigned char	uint8_t;
 typedef unsigned short	uint16_t;
 typedef short			int16_t;
-typedef unsigned long	uint32_t;
-typedef long			int32_t;
+typedef unsigned int	uint32_t;
+typedef int				int32_t;
 #else				/* Embedded platform */
 #include <stdint.h>
 #endif
