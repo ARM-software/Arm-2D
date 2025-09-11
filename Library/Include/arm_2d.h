4733e8bc081d4e6ef1600f14885d1d5d895da6ef
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-09-12 00:19:29 +0100

    minor update

--- Library/Include/arm_2d.h
+++ Library/Include/arm_2d.h
@@ -88,7 +88,7 @@ extern "C" {
 #define ARM_2D_VERSION_MAJOR        1                                           //!< Major version
 #define ARM_2D_VERSION_MINOR        2                                           //!< Minor version
 #define ARM_2D_VERSION_PATCH        4                                           //!< Patch number
-#define ARM_2D_VERSION_STR          "dev"                                          //!< tag
+#define ARM_2D_VERSION_STR          ""                                          //!< tag
 
 /*!
  * \brief arm-2d version number in decimal
