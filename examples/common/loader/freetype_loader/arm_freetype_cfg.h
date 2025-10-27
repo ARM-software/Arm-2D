6a17b4daffa64a88f9da7b48ed0a0e7860aa5a35
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-10-27 19:42:17 +0000

    add an option to enable CFF support

--- examples/common/loader/freetype_loader/arm_freetype_cfg.h
+++ examples/common/loader/freetype_loader/arm_freetype_cfg.h
@@ -7,6 +7,20 @@
 #include "arm_2d_cfg.h"
 
 
+//-------- <<< Use Configuration Wizard in Context Menu >>> -----------------
+//
+// <h>Arm-2D Customized FreeType Library Configuration
+// =======================
+//
+
+// <q> Support CFF (*.cff | *.otf)
+// <i> Support Compact Font Format (which requires pasux internally)
+#ifndef ARM_FREETYPE_CFG_SUPPORT_CFF
+#   define ARM_FREETYPE_CFG_SUPPORT_CFF       1
+#endif
+
+// <<< end of configuration section >>>
+
 
 #endif
 
