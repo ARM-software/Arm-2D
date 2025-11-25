322a5a23421806210c6f19e73f81d45a5368b8a4
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-11-25 07:20:37 +0000

    Update __arm_2d_impl.h

--- Library/Include/__arm_2d_impl.h
+++ Library/Include/__arm_2d_impl.h
@@ -22,7 +22,7 @@
  * Description:  header files for internal users or professional developers
  *
  * $Date:        25 Nov 2025
- * $Revision:    V.1.7.5
+ * $Revision:    V.1.7.6
  *
  * Target Processor:  Cortex-M cores
  *
@@ -1469,6 +1469,18 @@ arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_mask(
                                         __arm_2d_sub_task_t *ptTask);
 
 extern 
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_only(
+                                        __arm_2d_sub_task_t *ptTask);
+
+extern 
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_only(
+                                        __arm_2d_sub_task_t *ptTask);
+
+extern 
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_only(
+                                        __arm_2d_sub_task_t *ptTask);
+
+extern 
 arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_a1_mask_and_opacity(
                                         __arm_2d_sub_task_t *ptTask);
 
@@ -1514,6 +1526,18 @@ arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_mask_and_opacity(
 
 extern 
 arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_mask_and_opacity(
+                                        __arm_2d_sub_task_t *ptTask);
+
+extern 
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_and_opacity_only(
+                                        __arm_2d_sub_task_t *ptTask);
+
+extern 
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_and_opacity_only(
+                                        __arm_2d_sub_task_t *ptTask);
+
+extern 
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_and_opacity_only(
                                         __arm_2d_sub_task_t *ptTask);
 
 extern
@@ -1597,6 +1621,77 @@ arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_mask_xy_mirror_and_opacity(
 
 extern
 arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_mask_xy_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_and_x_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_and_x_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_and_x_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_x_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_x_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_x_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+extern
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_and_y_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_and_y_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_and_y_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_y_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_y_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_y_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_and_xy_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_and_xy_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_and_xy_mirror(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_gray8_sw_colour_filling_with_masks_xy_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_rgb565_sw_colour_filling_with_masks_xy_mirror_and_opacity(
+                                                __arm_2d_sub_task_t *ptTask);
+
+extern
+arm_fsm_rt_t __arm_2d_cccn888_sw_colour_filling_with_masks_xy_mirror_and_opacity(
                                                 __arm_2d_sub_task_t *ptTask);
 
 extern
