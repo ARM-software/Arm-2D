18345586bbe997ad9b3af76c19db2624ff8637c5
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2025-12-29 05:00:17 +0000

    prepare for a new example control: waveform view

---
+++ examples/common/controls/waveform_view.c
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2009-2025 Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*============================ INCLUDES ======================================*/
+#define __GENERIC_LOADER_INHERIT__
+#define __WAVEFORM_VIEW_IMPLEMENT__
+#include "waveform_view.h"
+
+#if defined(RTE_Acceleration_Arm_2D_Helper_PFB) && defined(RTE_Acceleration_Arm_2D_Extra_Loader)
+
+#include <assert.h>
+#include <string.h>
+
+#if defined(__clang__)
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wunknown-warning-option"
+#   pragma clang diagnostic ignored "-Wreserved-identifier"
+#   pragma clang diagnostic ignored "-Wdeclaration-after-statement"
+#   pragma clang diagnostic ignored "-Wsign-conversion"
+#   pragma clang diagnostic ignored "-Wpadded"
+#   pragma clang diagnostic ignored "-Wcast-qual"
+#   pragma clang diagnostic ignored "-Wcast-align"
+#   pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#   pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#   pragma clang diagnostic ignored "-Wmissing-braces"
+#   pragma clang diagnostic ignored "-Wunused-const-variable"
+#   pragma clang diagnostic ignored "-Wmissing-declarations"
+#   pragma clang diagnostic ignored "-Wmissing-variable-declarations"
+#endif
+
+/*============================ MACROS ========================================*/
+
+#if __GLCD_CFG_COLOUR_DEPTH__ == 8
+
+
+#elif __GLCD_CFG_COLOUR_DEPTH__ == 16
+
+
+#elif __GLCD_CFG_COLOUR_DEPTH__ == 32
+
+#else
+#   error Unsupported colour depth!
+#endif
+
+#undef this
+#define this    (*ptThis)
+
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ PROTOTYPES ====================================*/
+ARM_NONNULL(1)
+static
+arm_2d_err_t __waveform_view_decoder_init(arm_generic_loader_t *ptObj);
+
+ARM_NONNULL(1, 2, 3)
+static
+arm_2d_err_t __waveform_view_draw(  arm_generic_loader_t *ptObj,
+                                    arm_2d_region_t *ptROI,
+                                    uint8_t *pchBuffer,
+                                    uint32_t iTargetStrideInByte,
+                                    uint_fast8_t chBitsPerPixel);
+
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ IMPLEMENTATION ================================*/
+
+ARM_NONNULL(1,2)
+arm_2d_err_t waveform_view_init(waveform_view_t *ptThis,
+                                waveform_view_cfg_t *ptCFG)
+{
+    assert(NULL != ptThis);
+    assert(NULL != ptCFG);
+    memset(ptThis, 0, sizeof(waveform_view_t));
+
+    //if (NULL != ptCFG) {
+        this.tCFG = *ptCFG;
+    //}
+
+    arm_2d_err_t tResult = ARM_2D_ERR_NONE;
+
+    do {
+        if (NULL == this.tCFG.ImageIO.ptIO) {
+            this.use_as__arm_generic_loader_t.bErrorDetected = true;
+            tResult = ARM_2D_ERR_IO_ERROR;
+            break;
+        }
+
+        arm_generic_loader_cfg_t tCFG = {
+            .bUseHeapForVRES = this.tCFG.bUseHeapForVRES,
+            .tColourInfo.chScheme = ARM_2D_COLOUR,
+            .bBlendWithBG = true,
+            .ImageIO = {
+                .ptIO = this.tCFG.ImageIO.ptIO,
+                .pTarget = this.tCFG.ImageIO.pTarget,
+            },
+
+            .UserDecoder = {
+                .fnDecoderInit = &__waveform_view_decoder_init,
+                .fnDecode = &__waveform_view_draw,
+            },
+
+            .ptScene = this.tCFG.ptScene,
+        };
+
+        tResult = arm_generic_loader_init(  &this.use_as__arm_generic_loader_t,
+                                            &tCFG);
+
+        if (tResult < 0) {
+            break;
+        }
+
+        this.tTile.tRegion.tSize = this.tCFG.tSize;
+        if ((0 == this.tTile.tRegion.tSize.iWidth)
+         || (0 == this.tTile.tRegion.tSize.iHeight)) {
+            tResult = ARM_2D_ERR_INVALID_PARAM;
+            break;
+        }
+
+    } while(0);
+
+    return tResult;
+
+}
+
+ARM_NONNULL(1)
+void waveform_view_depose( waveform_view_t *ptThis)
+{
+    assert(NULL != ptThis);
+
+    arm_generic_loader_depose(&this.use_as__arm_generic_loader_t);
+}
+
+ARM_NONNULL(1)
+void waveform_view_on_load( waveform_view_t *ptThis)
+{
+    assert(NULL != ptThis);
+    
+    arm_generic_loader_on_load(&this.use_as__arm_generic_loader_t);
+}
+
+ARM_NONNULL(1)
+void waveform_view_on_frame_start( waveform_view_t *ptThis)
+{
+    assert(NULL != ptThis);
+    
+    arm_generic_loader_on_frame_start(&this.use_as__arm_generic_loader_t);
+}
+
+ARM_NONNULL(1)
+void waveform_view_on_frame_complete( waveform_view_t *ptThis)
+{
+    assert(NULL != ptThis);
+
+    arm_generic_loader_on_frame_complete(&this.use_as__arm_generic_loader_t);
+}
+
+ARM_NONNULL(1)
+static
+arm_2d_err_t __waveform_view_decoder_init(arm_generic_loader_t *ptObj)
+{
+    assert(NULL != ptObj);
+
+    waveform_view_t *ptThis = (waveform_view_t *)ptObj;
+
+
+    return ARM_2D_ERR_NONE;
+}
+
+ARM_NONNULL(1, 2, 3)
+static
+arm_2d_err_t __waveform_view_draw(  arm_generic_loader_t *ptObj,
+                                    arm_2d_region_t *ptROI,
+                                    uint8_t *pchBuffer,
+                                    uint32_t iTargetStrideInByte,
+                                    uint_fast8_t chBitsPerPixel)
+{
+
+
+
+
+    return ARM_2D_ERR_NONE;
+}
+
+#if defined(__clang__)
+#   pragma clang diagnostic pop
+#endif
+
+#endif
